506e1dbb365ea7fec6ca186351e8ff5a5cc3a494
Amir Rahemi <a.rahemi.us@gmail.com>
2024-02-02 23:14:15 +0330

    add font & fixed date contex

--- src/components/clock/MainClock.tsx
+++ src/components/clock/MainClock.tsx
@@ -1,50 +1,77 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
-import clockImage from "../../assets/images/clock.png"
+import clockImage from "../../assets/images/clock.png";
 
-import "../../assets/styles/clock.css"
+import "../../assets/styles/clock.css";
 
-type Props = {}
+type Props = {};
 
 const MainClock = (props: Props) => {
-    useEffect(() => {
-        const deg = 6;
-    
-        const hr = document.querySelector("#hr") as HTMLDivElement;
-        const mn = document.querySelector("#mn") as HTMLDivElement;
-        const sc = document.querySelector("#sc") as HTMLDivElement;
-    
-        setInterval(() => {
-          let day = new Date();
-    
-          let hh = day.getHours() * 30;
-    
-          let mm = day.getMinutes() * deg;
-    
-          let ss = day.getSeconds() * deg;
-    
-          hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-          mn.style.transform = `rotateZ(${mm}deg)`;
-          sc.style.transform = `rotateZ(${ss}deg)`;
-        });
-      });
-    
-      return (
-        <div>
-          <div className="clock">
-            <img src={clockImage} alt="clock-image" />
-            <div className="hour">
-              <div className="hr" id="hr"></div>
-            </div>
-            <div className="min">
-              <div className="mn" id="mn"></div>
-            </div>
-            <div className="sec">
-              <div className="sc" id="sc"></div>
-            </div>
-          </div>
-        </div>
-      );
-    };
+  useEffect(() => {
+    const deg = 6;
 
-export default MainClock
+    const hr = document.querySelector("#hr") as HTMLDivElement;
+    const mn = document.querySelector("#mn") as HTMLDivElement;
+    const sc = document.querySelector("#sc") as HTMLDivElement;
+
+    setInterval(() => {
+      let day = new Date();
+
+      let hh = day.getHours() * 30;
+
+      let mm = day.getMinutes() * deg;
+
+      let ss = day.getSeconds() * deg;
+
+      hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+      mn.style.transform = `rotateZ(${mm}deg)`;
+      sc.style.transform = `rotateZ(${ss}deg)`;
+    });
+  });
+
+  //   Digital Time
+  const currDate = new Date().toLocaleDateString();
+  const currTime = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  let digital = time.toLocaleTimeString().replace("AM","").replace("PM","");
+  let hours_12  = time.toLocaleTimeString().replace(/[0-9]+/g,"").replace(":","").replace(":","");
+
+  return (
+    <div>
+      <div className="clock">
+        <img src={clockImage} alt="clock-image" />
+        <div className="hour">
+          <div className="hr" id="hr"></div>
+        </div>
+        <div className="min">
+          <div className="mn" id="mn"></div>
+        </div>
+        <div className="sec">
+          <div className="sc" id="sc"></div>
+        </div>
+      </div>
+
+      <p> Current Date is = {currDate} </p>
+      <p> Curremt Time is = {currTime} </p>
+      <span className="font-clock num">{digital}</span>
+      <span className="font-clock hours">{hours_12}</span>
+      <br />
+
+      <span>
+        <h1>{time.toDateString()}</h1>
+      </span>
+    </div>
+  );
+};
+
+export default MainClock;
