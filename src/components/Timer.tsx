a0c02fcb4d0b647d7fa9663150793e87b5a596f4
Amir Rahemi <a.rahemi.us@gmail.com>
2024-02-02 16:55:09 +0330

    add Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import cleanTime from '../services/utils/cleanTime';
+import { ReduxStoreState } from '../services/types'
+import { connect } from 'react-redux';
+
+type StoreProps = {
+  currentTime: number;
+};
+
+type Props = StoreProps;
+
+const Timer = (props: Props) => {
+  const { currentTime } = props;
+
+  return (
+    <div>
+      <h1>{cleanTime(currentTime)}</h1>
+    </div>
+  );
+};
+
+const mapStateToProps = ({
+  timer: { currentTime }
+}: ReduxStoreState): StoreProps => ({
+  currentTime
+});
+
+export default connect<StoreProps, ReduxStoreState>(mapStateToProps)(Timer);
