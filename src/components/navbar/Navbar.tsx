aac61c809c4b4d6d8fd937e9e36a9e8c26c8d505
Amir Rahemi <amirrahemi01@gmail.com>
2024-02-10 18:52:56 +0330

    hover changes for navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,9 +6,9 @@ type Props = {}
 const Navbar = (props: Props) => {
   return (
     <div className="navbar bg-gray-600 shadow-lg shadow-gray-500/50 fixed m-11 p-4 text-white bottom-0 rounded-2xl">
-        <Link className="p-1 transition hover:text-sky-400" to="/">Clock</Link>
-        <Link className="p-1" to="/StopWatch">StopWatch</Link>
-        <Link className="p-1" to="/Worldclock">WorldClock</Link>
+        <Link className="p-1 transition duration-500 hover:text-slate-950" to="/">Clock</Link>
+        <Link className="p-1 transition duration-500 hover:text-slate-950" to="/StopWatch">StopWatch</Link>
+        <Link className="p-1 transition duration-500 hover:text-slate-950" to="/Worldclock">WorldClock</Link>
     </div>
   )
 }
