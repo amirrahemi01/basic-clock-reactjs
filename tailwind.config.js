87653ba07f90926ca221396f84be565127e37003
Amir Rahemi <amirrahemi01@gmail.com>
2024-02-11 21:57:11 +0330

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'slate': {
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155',
+        800: '#1e293b',
+        900: '#0f172a',
+        950: '#020617',
+      },
+      'gray': {
+        500: '#6b7280',
+        600: '#4b5563',
+        800: '#021500',
+      },
+      'green': {
+        700: '#0B2A13',
+      },
+      'red': {
+        600: '#350E0C',
+      },
+    },
     fontSize: {
       sm: '0.8rem',
       base: '1rem',
