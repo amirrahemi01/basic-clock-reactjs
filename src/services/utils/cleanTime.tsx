9d1b2a0a8e9b625146f7f28ff8e2208a24949d54
Amir Rahemi <amirrahemi01@gmail.com>
2024-02-11 21:59:45 +0330

    fix bug

--- src/services/utils/cleanTime.tsx
+++ src/services/utils/cleanTime.tsx
@@ -1,10 +1,15 @@
 const cleanTime = (timeInMs: number) => {
-    let centiseconds = ('00' + (Math.floor(timeInMs) % 1000)).slice(-3);
+    let centiseconds = ('0' + (Math.floor(timeInMs / 10) % 100)).slice(-2);
     let seconds = ('0' + (Math.floor(timeInMs / 1000) % 60)).slice(-2);
     let minutes = ('0' + (Math.floor(timeInMs / 60000) % 60)).slice(-2);
     let hours = ('0' + Math.floor(timeInMs / 3600000)).slice(-2);
 
-    return ` ${hours} : ${minutes} : ${seconds} : ${centiseconds} `;
+    return  (
+        <>
+        {hours} : {minutes} : {seconds} .  <span style={{opacity: 0}}>00</span> <span className="centi"> {centiseconds}</span>
+        </>
+    ) ;
+
 }
 
 export default cleanTime;
